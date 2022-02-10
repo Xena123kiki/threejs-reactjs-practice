8c3a2b7711a16addd83e0ef5066ea3cf42bb1b97
Xena123kiki <ckdnwls1215@snu.ac.kr>
2022-02-10 13:34:16 +0900

    fix: react hook

--- src/App.js
+++ src/App.js
@@ -1,38 +1,92 @@
-import {React, Component} from "react";
-import ReactDOM from "react-dom";
+// todo: this is class
+// import {React, Component} from "react";
+// import ReactDOM from "react-dom";
+// import * as THREE from "three";
+
+// class App extends Component {
+//   componentDidMount() {
+//       var scene = new THREE.Scene();
+//       var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+//       var renderer = new THREE.WebGLRenderer();
+//       renderer.setSize( window.innerWidth, window.innerHeight );
+//       this.mount.appendChild( renderer.domElement );
+    
+//       var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+//       var material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
+//       var cube = new THREE.Mesh( geometry, material );
+//       scene.add( cube );
+//       const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+//       scene.add( light );
+      
+//       camera.position.z = 2;
+//       var animate = function () {
+//           requestAnimationFrame( animate );
+//           cube.rotation.x += 0.01;
+//           cube.rotation.y += 0.01;
+//           cube.rotation.z += 0.01;
+//           renderer.render( scene, camera );
+//       };
+//       animate();
+//   }
+//   render() {
+//       return (
+//           <div ref={ref => (this.mount = ref)} />
+//       )
+//   }
+// }
+// const rootElement = document.getElementById("root")
+// ReactDOM.render(<App />, rootElement);
+// export default App;
+
+// todo: this is function
+import { useEffect, useRef } from "react";
 import * as THREE from "three";
 
-class App extends Component {
-  componentDidMount() {
-      var scene = new THREE.Scene();
-      var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-      var renderer = new THREE.WebGLRenderer();
+const App = () => {
+
+  const mountRef = useRef(null);
+
+  useEffect(() => {
+
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    var renderer = new THREE.WebGLRenderer();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    mountRef.current.appendChild( renderer.domElement );
+
+    var geometry = new THREE.BoxGeometry( 2, 2, 2 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var cube = new THREE.Mesh( geometry, material );
+
+    scene.add( cube );
+    camera.position.z = 5;
+
+    var animate = function () {
+      requestAnimationFrame( animate );
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render( scene, camera );
+    }
+
+    let onWindowResize = function () {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
       renderer.setSize( window.innerWidth, window.innerHeight );
-      this.mount.appendChild( renderer.domElement );
-    
-      var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-      var material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
-      var cube = new THREE.Mesh( geometry, material );
-      scene.add( cube );
-      const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
-      scene.add( light );
-      
-      camera.position.z = 2;
-      var animate = function () {
-          requestAnimationFrame( animate );
-          cube.rotation.x += 0.01;
-          cube.rotation.y += 0.01;
-          cube.rotation.z += 0.01;
-          renderer.render( scene, camera );
-      };
-      animate();
-  }
-  render() {
-      return (
-          <div ref={ref => (this.mount = ref)} />
-      )
-  }
+    }
+
+    window.addEventListener("resize", onWindowResize, false);
+
+    animate();
+
+    return () => mountRef.current.removeChild( renderer.domElement);
+  }, []);
+
+  return (
+    <div ref={mountRef}>
+        <h1 style={{marginLeft: '15px', fontWeight: 900}}>THIS IS THREEJS AND REACTJS PRACTICE</h1>    
+    </div>
+  );
 }
-const rootElement = document.getElementById("root")
-ReactDOM.render(<App />, rootElement);
+
 export default App;
